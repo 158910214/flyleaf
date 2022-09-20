b21c06a9ac8b179279a2fd4abd5797aa37e661aa
yesc <158910214@qq.com>
2022-09-20 21:52:43 +0800

    feat: 重构 hollow-mask

--- src/components/Hollow-Mask/Hollow-Mask.svelte
+++ src/components/Hollow-Mask/Hollow-Mask.svelte
@@ -3,67 +3,67 @@
 	import { useRect, useDocumentRect } from '@/util/useRect'
 	import Teleport from '../teleport/Teleport.svelte'
 	import ToolTip from '../tooltip/Tooltip.svelte'
+	import { tick } from 'svelte'
 
 	export let show = false
 	export let dom = null
 	export let gutter = 20
 
-	let HollowWrapEl
-	let style = ''
-	let cntStyle = ''
-	let cloneNodeArea
-	let curChild = null
+	let highlightedStyle = ''
 
-	const initDom = () => {
+	const initDom = async () => {
 		if (dom) {
 			const curDom = typeof dom === 'string' ? document.querySelector(dom) : dom
 			const { width, height, top, left } = useRect(curDom)
-			console.log(width, height, top, left)
 			const bdTop = Math.ceil(top)
 			if (!curDom) {
 				show = false
 				return console.log('can not find current dom')
 			}
-			if (!cloneNodeArea) {
-				show = false
-				return console.log('can not find cloneNodeArea')
+			console.log(bdTop)
+			console.log(Math.ceil(left))
+			if (
+				!['hollow-position-relative', 'hollow-highlighted-element'].some(
+					(cls) => curDom.className.split('').includes(cls)
+				)
+			) {
+				curDom.className +=
+					'hollow-position-relative hollow-highlighted-element '
 			}
-			if (!curChild) {
-				curChild = curDom.cloneNode(true)
-				cloneNodeArea.appendChild(curChild)
-			}
-			style = [
+			highlightedStyle = [
 				'width:' + Math.ceil(width) + 'px;',
 				'height:' + Math.ceil(height) + 'px;',
-				'top:' + bdTop + 'px;',
-				'left:' + Math.ceil(left) + 'px;',
+				'top:' + (bdTop - gutter) + 'px;',
+				'left:' + (Math.ceil(left) - gutter) + 'px;',
 				'border:' + gutter + 'px solid #fff;',
-			].join('')
-			cntStyle = [
-				'top:' + (height + bdTop + gutter * 2 + 10) + 'px;',
-				'left:' + (Math.ceil(left) + gutter) + 'px;',
 			].join('')
 		}
 	}
 
-	$: if (dom && show === true && cloneNodeArea) {
+	$: if (dom && show === true) {
 		initDom()
 	}
 	onMount(() => show === true && initDom())
 </script>
 
-{#if show}
-	<Teleport>
-		<div class="hollow-mask" pos-fixed left-0 right-0 bottom-0 top-0>
-			<div pos-fixed left-0 right-0 bottom-0 top-0 bg-op-50 z-3000 bg-black />
-			<div pos-fixed z-3001 bg-white bind:this={HollowWrapEl} {style}>
-				<ToolTip alwaysShow={true}>
-					<div bind:this={cloneNodeArea} />
-					<div slot="content" bg-white>
-						<slot />
-					</div>
-				</ToolTip>
-			</div>
-		</div>
-	</Teleport>
-{/if}
+<svelte:head>
+	<style>
+		.hollow-highlighted-element {
+			z-index: 100004 !important;
+		}
+		.hollow-position-relative {
+			position: relative !important;
+		}
+	</style>
+</svelte:head>
+
+<!-- driver-page-overlay -->
+<Teleport>
+	<div class="fixed left-0 right-0 bottom-0 top-0 bg-op-80 z-3000 bg-black">
+		<div
+			class="fixed left-0 right-0 bottom-0 top-0 bg-op-80 z-3000 bg-black "
+		/>
+		<!-- driver-highlighted-element-stage -->
+		<div class="pos-relative bg-white z-1000005" style={highlightedStyle} />
+	</div>
+</Teleport>
